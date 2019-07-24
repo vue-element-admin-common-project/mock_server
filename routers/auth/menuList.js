26a5852851fdb23faa1690c7b90fce8a15352a90
chen1zee <chen2zee1@gmail.com>
2019-07-24 13:07:16 +0800

    [init]07_24_01

--- routers/auth/menuList.js
+++ routers/auth/menuList.js
@@ -8,7 +8,7 @@ const level1IDs = {
 }
 
 module.exports = [
-    { id: level1IDs.agent, level: 1, menuName: '代理商管理', parentId: 0, path: '/agent', types: 0 },
-    { id: cnt++, level: 2, menuName: '代理商列表', parentId: level1IDs.agent, path: '/agent/list', types: 0 },
-    { id: cnt++, level: 2, menuName: '代理商订单列表', parentId: level1IDs.agent, path: '/agent/orderList', types: 0 },
+    { id: level1IDs.agent, level: 1, menuName: '店家管理', parentId: 0, path: '/agent', types: 0 },
+    { id: cnt++, level: 2, menuName: '店家列表', parentId: level1IDs.agent, path: '/agent/list', types: 0 },
+    { id: cnt++, level: 2, menuName: '店家订单列表', parentId: level1IDs.agent, path: '/agent/orderList', types: 0 },
 ]
